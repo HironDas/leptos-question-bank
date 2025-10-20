dd17867018eeb335bcf73354ceac12049f6bcd92
Hiron Das <hcdas.09@gmail.com>
2025-10-20 18:45:55 +0600

    add init sqlx migrations and bd scripts

---
+++ migrations/20251020120228_create_profile_table.sql
@@ -0,0 +1,13 @@
+-- Add migration script here
+-- Create Profile table
+CREATE TABLE IF NOT EXISTS profiles (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER UNIQUE NOT NULL,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    bio TEXT,
+    avatar_url VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
