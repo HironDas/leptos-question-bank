569082efea266568216ce80380357808ca7cea19
Hiron Das <hcdas.09@gmail.com>
2025-10-27 11:48:42 -0400

    finally the signup is done

--- src/pages/signup.rs
+++ src/pages/signup.rs
@@ -1,59 +1,49 @@
-use crate::server_function::signup;
 use leptos::prelude::*;
 use singlestage::*;
+use crate::server_function::signup::*;
 
 #[component]
 pub fn Signup() -> impl IntoView {
     let signup = ServerAction::<Signup>::new();
+    let _value = signup.value();
 
     view! {
         <div class="flex flex-col items-center justify-center min-h-screen px-4">
-            <ActionForm action=signup>
-        <Card class="w-full sm:w-sm">
-            <CardHeader>
-                <CardTitle>"Create new account"</CardTitle>
-                <CardDescription>
-                    "Enter your details below to create a new account"
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <div class="form grid gap-6">
-                    <div class="grid gap-2">
-                        <Label label_for="demo-card-form-username">"Username"</Label>
-                        <Input name="username" input_type="text" id="demo-card-form-username" />
+        <ActionForm action=signup>
+            <Card class="w-full sm:w-sm">
+                <CardHeader>
+                    <CardTitle>"Create new account"</CardTitle>
+                    <CardDescription>
+                        "Enter your details below to create a new account"
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <div class="form grid gap-6">
+                        <div class="grid gap-2">
+                            <Label label_for="demo-card-form-username">"Username"</Label>
+                            <Input name="username" input_type="text" id="demo-card-form-username" />
+                        </div>
+                        <div class="grid gap-2">
+                            <Label label_for="demo-card-form-email">"Email"</Label>
+                            <Input name="email" input_type="email" id="demo-card-form-email" />
+                        </div>
+                        <div class="grid gap-2">
+                            <Label label_for="demo-card-form-password">"Password"</Label>
+                            <Input name="password" input_type="password" id="demo-card-form-password" />
+                        </div>
+                        <div class="grid gap-2">
+                            <Label label_for="demo-card-form-cpassword">"Confirm Password"</Label>
+                            <Input name="confirm_password" input_type="password" id="demo-card-form-cpassword" />
+                        </div>
                     </div>
-                    <div class="grid gap-2">
-                        <Label label_for="demo-card-form-email">"Email"</Label>
-                        <Input name="email" input_type="email" id="demo-card-form-email" />
-                    </div>
-                    <div class="grid gap-2">
-                        <Label label_for="demo-card-form-password">"Password"</Label>
-                        <Input name="password" input_type="password" id="demo-card-form-password" />
-                    </div>
-                    <div class="grid gap-2">
-                        <Label label_for="demo-card-form-cpassword">"Confirm Password"</Label>
-                        <Input name="confirm_password" input_type="password" id="demo-card-form-cpassword" />
-                    </div>
-                </div>
-            </CardContent>
-            <CardFooter class="flex flex-col items-center gap-2">
-                <Button button_type="submit" class="w-full">
-                    "Sign up"
-                </Button>
-            </CardFooter>
-        </Card>
-            </ActionForm>
+                </CardContent>
+                <CardFooter class="flex flex-col items-center gap-2">
+                    <Button button_type="submit" class="w-full">
+                        "Sign up"
+                    </Button>
+                </CardFooter>
+            </Card>
+        </ActionForm>
         </div>
     }
-}
-
-#[server]
-async fn signup(
-    username: String,
-    email: String,
-    password: String,
-    confirm_password: String,
-) -> Result<(), ServerFnError> {
-    // TODO: Implement signup logic
-    Ok(())
 }
--- src/server_function/signup.rs
+++ src/server_function/signup.rs
@@ -1,6 +1,5 @@
 use leptos::prelude::*;
 
-#[cfg(feature = "ssr")]
 #[server]
 pub async fn signup(
     username: String,
@@ -8,26 +7,29 @@ pub async fn signup(
     password: String,
     confirm_password: String,
 ) -> Result<(), ServerFnError> {
-    use sqlx::PgPool;
-    use std::sync::Arc;
-
-    print!("Singn up function is called!!");
+    leptos::logging::log!("Signup is clicked --");
     use crate::domain::new_user::NewUser;
-
-    let pool = expect_context::<Arc<PgPool>>();
     let user: NewUser = NewUser::new(username, email, password, confirm_password);
     let password_hash = user.hash_password();
+    
+    #[cfg(feature = "ssr")]{
 
-    sqlx::query(
-        r#"
-        INSERT INTO users (username, email, password_hash)
-        VALUES ($1, $2, $3)
-        "#,
-    )
-    .bind(user.username.as_ref())
-    .bind(user.email.as_ref())
-    .bind(&password_hash)
-    .execute(&*pool)
-    .await?;
+        use sqlx::PgPool;
+        use std::sync::Arc;
+    
+        let pool = expect_context::<Arc<PgPool>>();
+    
+        sqlx::query(
+            r#"
+            INSERT INTO users (username, email, password_hash)
+            VALUES ($1, $2, $3)
+            "#,
+        )
+        .bind(user.username.as_ref())
+        .bind(user.email.as_ref())
+        .bind(&password_hash)
+        .execute(&*pool)
+        .await?;
+    }
     Ok(())
 }
